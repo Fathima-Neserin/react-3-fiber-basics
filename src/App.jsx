2fc0cd40b242a5d4b99a51d41d9d104f061212ec
fathima <fathimanezrin2001@gmail.com>
2024-11-30 21:47:26 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Canvas, useFrame } from '@react-three/fiber'
 import './App.css'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
       {/* mesh is the fundamental building block of any 3D geometry objects */}
 const Cube = ({position, size, color}) => {
@@ -10,13 +10,15 @@ const Cube = ({position, size, color}) => {
   useFrame((state, delta) => {
     ref.current.rotation.x += delta
     ref.current.rotation.y += delta * 2.0
-    ref.current.position.z += delta
+    ref.current.position.z += Math.sin(state.clock.elapsedTime) * 0.09
 
     // console.log(delta);
     // console.log(state);
+    // console.log(state.clock.elapsedTime);
+    
     
   })
-  // delta : value of time of rotation on each direction
+  // delta : value of time of rotation on each direction(0.something something)
   // state : object with loof values such that camera, timeStamp, interval and so on
   return(
     
@@ -26,6 +28,76 @@ const Cube = ({position, size, color}) => {
   </mesh>
   )
 }
+
+const Sphere = ({position, size, color}) => {
+
+  const [hovered, setHovered] = useState(false);  
+  const [clicked, setClicked] = useState(false); 
+  const ref = useRef();
+
+  useFrame((state, delta) => {
+    // ref.current.rotation.x += delta * 0.2
+    const speed = hovered ? 1 : 0.2 
+    ref.current.rotation.y += delta * speed
+    // ref.current.position.z += Math.sin(state.clock.elapsedTime) * 0.04
+    
+  })
+  return(
+    
+    <mesh 
+    position={position} 
+    ref={ref}
+    onPointerEnter={(e) => {e.stopPropagation(), setHovered(true)}}
+    onPointerLeave={(e) => setHovered(false)}
+    onClick={() => setClicked(true)}
+    scale={clicked ? 1.5 : 1}
+    >
+      <sphereGeometry size={size}/>
+      <meshStandardMaterial color={hovered ? "orange" : "lightblue"} wireframe/>
+    </mesh>
+  )
+}
+
+
+const Torus = ({position, size, color}) => {
+  
+  const ref = useRef();
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta
+    ref.current.rotation.y += delta * 4
+    ref.current.position.z += Math.sin(state.clock.elapsedTime) * 0.06
+    
+})
+  return(
+    
+    <mesh position={position} ref={ref}>
+      <torusGeometry args={size}/>
+      <meshStandardMaterial color={color}/>
+    </mesh>
+  )
+}
+
+
+const TorusKnot = ({position, size, color}) => {
+  
+  const ref = useRef();
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta
+    ref.current.rotation.y += delta * 5
+    ref.current.position.z += Math.sin(state.clock.elapsedTime) * 0.08
+    
+})
+  return(
+    
+    <mesh position={position} ref={ref}>
+      <torusKnotGeometry args={size}/>
+      <meshStandardMaterial color={color}/>
+    </mesh>
+  )
+}
+
 
 const App = () => {
 
@@ -46,7 +118,11 @@ const App = () => {
        <Cube position={[1, 2, 0]} color={"yellow"} size={[1, 1, 1]}/>
        </group> */}
     
-    <Cube position={[0, 0, 0]} color={"red"} size={[1, 1, 1]}/>
+    {/* <Cube position={[0, 0, 0]} color={"red"} size={[1, 1, 1]}/> */}
+       
+       <Sphere position={[0, 0, 0]} size={[1, 30, 30]} color={"orange"}/>
+       {/* <Torus position={[2, 0, 0]} size={[0.8, 0.1, 30, 30]} color={"pink"}/>
+       <TorusKnot position={[-2, 0, 0]} size={[0.5, 0.1, 1000, 50]} color={"brown"}/> */}
     </Canvas>
     </>
   )
